34f69dcad6b903f2a62f4ffa2921ec275a3887e5
yusufabadikun <112902284+yusufabadikun@users.noreply.github.com>
2022-09-06 23:29:49 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="$nitroINU" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          an Arbitrum Nitro Memecoin
         </p>
       </main>
 
